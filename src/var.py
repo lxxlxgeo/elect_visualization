5ecf22d3ff0a7346275bd5d1fdc39a0d310ab518
Clee <854967846@qq.com>
2023-07-26 10:50:32 +0800

    old

---
+++ src/var.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 13:42:50 2023
+
+@author: my_pc
+"""
+
+province_path = 'E:/workproject/elect_visualization/old/EC_D1D_PLOT/heilongjiang_shp/heilongjiang_city.shp'
+
+city_path = "E:/workproject/elect_visualization/old/EC_D1D_PLOT/heilongjiang_shp/heilongjiang_city.shp"
--- visual/visualbase.py
+++ visual/visualbase.py
@@ -26,8 +26,52 @@ from matplotlib import colors
 import cmaps
 import matplotlib.colors as mcolors
 
+import xarray as xr
+import cfgrib
 
 # %%
+FILE = r"G:\js_elect\ECFile\C1D\2023072112\W_NAFP_C_ECMF_20230721181044_P_C1D07211200072418001\W_NAFP_C_ECMF_20230721181044_P_C1D07211200072418001"
+
+# xr.open_dataset(FILE,engine='cfgrib',backend_kwargs={'filter_by_keys':{'typeOfLevel':'surface'}, {'edition': 1}})
+# p={'filter_by_keys':{'typeOfLevel':'surfacce'},{'edition':1}}
+
+ds = xr.open_dataset(FILE, engine='cfgrib', backend_kwargs={
+                     'filter_by_keys': {'typeOfLevel': 'surface', 'edition': 1}})
+
+t2m = ds['t2m']
+
+# %%
+# 数据读取部分
+
+
+class Readgrib2Tiff(object):
+    def __init__(self, file):
+        self.file = file
+        self.filename = os.path.basename(file)
+
+        self.ds = xr.open_dataset(file, engine='cfgrib')
+        self.get_extent()
+
+    def get_extent(self):
+        lat = self.ds['latitude'].to_numpy()
+        lon = self.ds['longitude'].to_numpy()
+
+        latmin = round(lat.min(), 2)
+        latmax = round(lat.max(), 2)
+
+        lonmin = round(lon.min(), 2)
+        lonmax = round(lon.max(), 2)
+
+        self.extext = [lonmin, lonmax, latmin, latmax]
+        del lat, lon
+
+    def Filter_Context(self):
+        # 选择不同的矢量文件
+        pass
+
+# %%
+
+
 def normal_plot_scatter_img(extents, data, lon, lat, filename, title, norm, colormap, level):
     '''
     :param extents: 坐标范围
@@ -75,3 +119,13 @@ def normal_plot_scatter_img(extents, data, lon, lat, filename, title, norm, colo
     plt.savefig(filename, format='png', bbox_inches='tight', dpi=600)
     plt.close()
     del ax, im2, fig
+
+
+# %%
+colormap = cmaps.t2m_29lev
+proj = ccrs.PlateCarree()
+fig = plt.figure(dpi=600)
+ax = fig.add_subplot(111, projection=proj)
+
+im2 = ax.contourf(t2m.longitude, t2m.latitude, t2m,
+                  cmap=colormap, extend="both")
