d658346f41dd5152e5d171fcb84c8f535aa2f849
Clee <854967846@qq.com>
2023-08-13 02:30:15 +0800

    提交一版 RLDAS 的处理

--- RLDAS_self/RLdas_Read.py
+++ RLDAS_self/RLdas_Read.py
@@ -20,8 +20,9 @@ class RlDas_Reader(object):
         self.start_time = start_time
         self.end_time = end_time  # 结束时间
         self.step = step  # 预报步长
-        self.filepath = rldas_path # rldas 文件路径
+        self.filepath = rldas_path  # rldas 文件路径 这里需要改下
         self.extent = extent       # 出图的范围
+
         self.forecast_time_list = self.__get_forecast_time_list__()  #获取时间序列
 
     def __get_forecast_time_list__(self):
@@ -29,12 +30,10 @@ class RlDas_Reader(object):
         这里规定,如果间隔时常为24小时的话，开始时间到结束时间不足
         :return:
         """
-        # time_length=self.end_time-self.start_time
         time_length = self.end_time - self.start_time
         days = time_length.days
         seconds = time_length.seconds
         hours = days * 24 + seconds / 3600  # 计算预报总时长
-
         if hours % self.step != 0:
             print("当前实况结束时间设置错误，已自动更正")
             self.end_time = self.start_time + datetime.timedelta(hours=(days * 24 + self.step))
@@ -61,11 +60,11 @@ class RlDas_Reader(object):
         """
         if attribute=="RAINC":
             # 如果输入的变量为降水,则获取的文件为 前一时刻的降水量
-            pre_forecast_time_str=forecast_time+datetime.timedelta(hours=self.step)
+            pre_forecast_time_str=forecast_time+datetime.timedelta(hours=-self.step)
+
         else:
             #新建一个列表,里面存储检索到的文件
             file_list=[]
-            #forecast_time_str=forecast_time
             for i in range(self.step):
                 # 暂时先这样吧,以后再改
                 temp_time_str = (forecast_time + datetime.timedelta(hours=i + 1)).strftime('%Y%m%d%H')
@@ -75,9 +74,6 @@ class RlDas_Reader(object):
                 else:
                     print("当前时段:{} 没有数据,可能造成计算错误".format(temp_time_str))
                     continue
-
-
-
 
         pass
 
