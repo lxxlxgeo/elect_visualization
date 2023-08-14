521c1d79ad173fd08ac0a2930fdd7fe6f6d65bae
Clee <854967846@qq.com>
2023-08-14 11:56:15 +0800

    签出新分支,提交更改

--- cldas/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2023/8/12 2:59
-
-@author: lxce
-"""
--- start.sh
+++
@@ -1,2 +0,0 @@
-cd /home/groupwang/software/lxdir/elect_product_run
-/home/groupwang/anaconda3/envs/py39_kt/bin/python exec_main.py
